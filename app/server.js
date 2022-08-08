50b041124076ddbea54a50b1966bcc2106766d00
Hayatu Abubakar <hayatu2@gmail.com>
2022-08-08 17:27:01 +0100

    feat: implemented rate-limiting

--- app/server.js
+++ app/server.js
@@ -1,50 +1,23 @@
-var express = require('express'),
-  api = express(),
-  port = process.env.PORT || 3000;
-  bodyParser = require('body-parser');
+const express = require('express');
+const port = process.env.PORT || 3000;
+const bodyParser = require('body-parser');
+const rateLimit = require('express-rate-limit');
 
-  //rate limiting bucket
-  class TokenBucket{
-    constructor(capacity, perSecond){
-      this.capacity = capacity;
-      this.tokens = capacity;
-      setInterval(()=> this.putToken(), 1000*15/perSecond);
-    }
-    putToken(){
-      if (this.tokens < this.capacity) {
-        console.log("Token Added")
-        this.tokens += 1;
-      }
-    }
-    removeToken(){
-      if (this.tokens > 0) {
-        console.log("Token Removed")
-        this.tokens -= 1;
-        return true;
-      }
-      return false;
-    }
-  }
-  //rate-limiting
-  function rateLimiting() {
-    const bucket = new TokenBucket(process.env.RATE_PER_SECOND, process.env.MAX_BURST);
-    return function limitRequestsMiddleware(req,res,next) {
-      if (bucket.take()) {
-        next();
-      }else{
-        res.status(429).send("Rate limit Exceeded. Try again in a few minutes");
-      }
-    }
-  }
-  api.use(bodyParser.urlencoded({ extended: true }));
-  api.use(bodyParser.json());
+const api = express();
+const limit = rateLimit({
+  windowMs: 10 * 1000,
+  max: 3 
+});
+api.use(limit);
+api.use(bodyParser.urlencoded({ extended: true }));
+api.use(bodyParser.json());
  
-  var routes = require('./api/routes/serverRoutes');
-  routes(api);
-  api.use(function(req, res) {
-    rateLimiting();
-    res.status(404).send({url: req.originalUrl + ' not found'})
-  });
+const routes = require('./api/routes/serverRoutes');
+routes(api);
+api.use(function(req, res) {
+  rateLimiting();
+  res.status(404).send({url: req.originalUrl + ' not found'})
+});
 api.listen(port);
 
 console.log('API Server Started on: ' + port);
