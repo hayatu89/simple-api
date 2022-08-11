67a0191c247f3d413a84fa111090f986b7b82201
Hayatu Abubakar <hayatu2@gmail.com>
2022-08-11 05:26:13 +0100

    chore: fixing codacy issues

--- app/api/controllers/serverController.js
+++ app/api/controllers/serverController.js
@@ -47,7 +47,7 @@ function dateValidation(dateString){
         let day = parseInt(datepart[2], 10);      
                  
         let ListofDays = [31,28,31,30,31,30,31,31,30,31,30,31];      
-        if (month==1 || month>2){      
+        if (month===1 || month>2){      
             if (day>ListofDays[month-1]){           
                 return false;      
             }      
@@ -56,10 +56,10 @@ function dateValidation(dateString){
             if ( (!(year % 4) && year % 100) || !(year % 400)) {      
                 leapYear = true;      
             }      
-            if ((leapYear == false) && (day>=29)){      
+            if ((leapYear === false) && (day>=29)){      
                 return false;      
             }else      
-            if ((leapYear==true) && (day>29)){          
+            if ((leapYear===true) && (day>29)){          
                 return false;      
             }      
         }      
