a86de0b1625409bc11c6bcfc52d1f0f7b10c325a
Hayatu Abubakar <hayatu2@gmail.com>
2022-09-17 18:11:04 +0100

    chore: rafactor to get a better score for codacy

--- app/api/routes/serverRoutes.js
+++ app/api/routes/serverRoutes.js
@@ -2,6 +2,5 @@
 
 module.exports = function (api) {
     const simpleAPI = require('../controllers/serverController');
-    api.route('/howold')
-        .get(simpleAPI.howOld);
+    api.route('/howold').get(simpleAPI.howOld);
 }
