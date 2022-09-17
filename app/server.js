7e0ba513d4b7a1dac81be5629759d1649de7449c
Hayatu Abubakar <hayatu2@gmail.com>
2022-09-17 15:38:35 +0100

    feat: changed rate limiter

--- app/server.js
+++ app/server.js
@@ -1,16 +1,40 @@
 const express = require('express');
 const port = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
-const rateLimit = require('express-rate-limit');
+class RateLimiter {
+  constructor(capacity, fill_per_second){
+      this.capacity = capacity;
+      this.tokens = capacity;
+      setInterval(()=> this.addToken(), 1000 * 20/fill_per_second);
+  }
+
+  addToken(){
+      if (this.tokens < this.capacity) {
+          this.tokens += 1;
+      }
+  }
+  removeToken(){
+      if (this.tokens > 0) {
+          this.tokens -= 1;
+          return true;
+      }
+      return false;
+  }
+}
 
 const api = express();
-const limit = rateLimit({
-  windowMs: 1000,
-  max: 3,
-  standardHeaders: true,
-  legacyHeaders: false, 
-});
-api.use(limit);
+
+function limitRequests(perSecond, maxBurst) {
+  const bucket = new RateLimiter(3,2);
+  return function limitRequestsMiddleware(req, res, next) {
+      if (bucket.removeToken()) {
+          next();
+      } else {
+          res.status(429).send('Rate limit exceeded');
+      }
+  }
+}
+api.use(limitRequests(3,3));
 api.use(bodyParser.urlencoded({ extended: true }));
 api.use(bodyParser.json());
  
